866d15cc07cb61724cc875be99539b34aaaa200d
Deepak Srinivas <68526096+Homunculus-code@users.noreply.github.com>
2023-03-17 19:33:49 +0530

    Update Ace.js

--- src/components/Ace/Ace.js
+++ src/components/Ace/Ace.js
@@ -13,37 +13,18 @@ const Ace = () => {
 	useEffect(() => {
         AOS.init();
       }, []);
-	  
-	const [windowDimension, setWindowDimension] = useState(null);
 
-	useEffect(() => {
-		setWindowDimension(window.innerWidth);
-	}, []);
-
-	useEffect(() => {
-		function handleResize() {
-		setWindowDimension(window.innerWidth);
-		}
-
-		window.addEventListener("resize", handleResize);
-		return () => window.removeEventListener("resize", handleResize);
-	}, []);
-
-	const isMobile = windowDimension <= 640;
-	if (!isMobile){
-		return(
-
+	return(
 			<div style={{ position: "relative", top: "50px" }} className={styles.ace }>
-				<div style = {{display: "flex", flexDirection: "column", gap: "200px"}}>
+				<div className = {styles.everything}>
 					<div>
 						<div className={styles.photoplusace }>
-							<div>
-								<img src = "/assets/whatisace.jpg" alt="whatisace" height = "20%" width = "60%" style = {{position: "relative", left: "260px"}}/>
-								<div className={styles.blurbox} style = {{position: "relative", top: "-70px", boxSizing: "border-box", width: "600px", height: "50px", padding: "50px", border: "100px",textAlign: "center", zIndex: 1}}>
-									<h1 className={`${styles.typing} ${styles.demo}`} style = {{position: "relative", left: "120px"}}>What is Ace?</h1>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia, nisl eu rhoncus auctor, lacus libero tincidunt dui, in consectetur lorem arcu ut ex. Praesent tristique nisi quis metus rutrum sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis pharetra mi. Curabitur et metus congue, aliquet metus sit amet, condimentum elit. Sed accumsan risus lacinia turpis accumsan pharetra. Nam et mi ipsum. Nam consectetur leo et libero viverra hendrerit. Cras vel purus dolor. Integer mattis erat a placerat porta.</p>
+								<img src = "/assets/whatisace.jpg" className = {styles.whatisaceimage} alt="whatisace" height = "20%" width = "60%" style = {{position: "relative", left: "260px"}}/>
+								<div className={styles.blurbox}>
+									<h1 className = {styles.whatisace}>What is Ace?</h1>
+									<div className = {styles.acepic}><img src = "https://i.ibb.co/WHmm2qh/logo-1.png" height = "100px" width = "100px"/></div>
+									<p className = {styles.acepara}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia, nisl eu rhoncus auctor, lacus libero tincidunt dui, in consectetur lorem arcu ut ex. Praesent tristique nisi quis metus rutrum sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis pharetra mi. Curabitur et metus congue, aliquet metus sit amet, condimentum elit. Sed accumsan risus lacinia turpis accumsan pharetra. Nam et mi ipsum. Nam consectetur leo et libero viverra hendrerit. Cras vel purus dolor. Integer mattis erat a placerat porta.</p>
 								</div>
-							</div>
 						</div>
 					</div>
 					<div style = {{display: "flex", gap: "100px", flexDirection: "column"}}>
@@ -52,11 +33,11 @@ const Ace = () => {
 						</div>
 						<div className={styles.team} style = {{display: "flex", gap: "20px", flexDirection: "column"}}>
 							<div>
-								<div style = {{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+								<div className = {styles.teamstruct}>
 									<div style = {{textAlign: 'center'}}>
 										<hr />
 										<h1>Team 2022 - 2023</h1>
-										<h4 style = {{width: "600px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia, nisl eu rhoncus auctor, lacus libero tincidunt dui, in consectetur lorem arcu ut ex. Praesent tristique nisi quis metus rutrum sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis pharetra mi. Curabitur et metus congue, aliquet metus sit amet, condimentum elit. Sed accumsan risus lacinia turpis accumsan pharetra.</h4>
+										<h4 className = {styles.para1}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia, nisl eu rhoncus auctor, lacus libero tincidunt dui, in consectetur lorem arcu ut ex. Praesent tristique nisi quis metus rutrum sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis pharetra mi. Curabitur et metus congue, aliquet metus sit amet, condimentum elit. Sed accumsan risus lacinia turpis accumsan pharetra.</h4>
 									</div>
 									<div className={styles.profile }>
 										{team.exeMembers.map(member => <p><AceCard pic = {member.pic} name = {member.name.toUpperCase()} position = {member.position} bio = {member.bio}/></p>)}
@@ -64,10 +45,10 @@ const Ace = () => {
 								</div>
 							</div>	
 							<div>
-								<div style = {{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+								<div className = {styles.teamstruct}>
 									<div style = {{textAlign: 'center'}}>
 										<h1>Design Team</h1>
-											<h4 style={{ width: "600px" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia, nisl eu rhoncus auctor, lacus libero tincidunt dui, in consectetur lorem arcu ut ex. Praesent tristique nisi quis metus rutrum sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis pharetra mi. Curabitur et metus congue, aliquet metus sit amet, condimentum elit. Sed accumsan risus lacinia turpis accumsan pharetra.</h4>
+											<h4 className = {styles.para1}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia, nisl eu rhoncus auctor, lacus libero tincidunt dui, in consectetur lorem arcu ut ex. Praesent tristique nisi quis metus rutrum sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis pharetra mi. Curabitur et metus congue, aliquet metus sit amet, condimentum elit. Sed accumsan risus lacinia turpis accumsan pharetra.</h4>
 									</div>
 									<div className={styles.profile }>
 										{team.desTeam.map(member => <p><AceCard pic = {member.pic} name = {member.name.toUpperCase()} position = {member.position} bio = {member.bio}/></p>)}
@@ -75,10 +56,10 @@ const Ace = () => {
 								</div>
 							</div>	
 							<div>
-								<div style = {{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+								<div className = {styles.teamstruct}>
 									<div style = {{textAlign: 'center'}}>
 										<h1>Web Team</h1>
-											<h4 style={{ width: "600px" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia, nisl eu rhoncus auctor, lacus libero tincidunt dui, in consectetur lorem arcu ut ex. Praesent tristique nisi quis metus rutrum sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis pharetra mi. Curabitur et metus congue, aliquet metus sit amet, condimentum elit. Sed accumsan risus lacinia turpis accumsan pharetra.</h4>
+											<h4 className = {styles.para1}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia, nisl eu rhoncus auctor, lacus libero tincidunt dui, in consectetur lorem arcu ut ex. Praesent tristique nisi quis metus rutrum sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis pharetra mi. Curabitur et metus congue, aliquet metus sit amet, condimentum elit. Sed accumsan risus lacinia turpis accumsan pharetra.</h4>
 									</div>
 									<div className={styles.profile }>
 										{team.webTeam.map(member => <p><AceCard pic = {member.pic} name = {member.name.toUpperCase()} position = {member.position} bio = {member.bio}/></p>)}
@@ -88,57 +69,8 @@ const Ace = () => {
 						</div>
 					</div>
 				</div>
-			</div>
-			
-		)
-	}
-	else{
-		return(
-			<>
-				<div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "10px" }} className={styles.ace}>
-					<img src = "https://acesvce.in/images/logo.png" alt="logo"/>	
-					<h2 class = "typing-demo" style = {{letterSpacing: '7px'}}>WHAT IS ACE?</h2>
-					<h6 style = {{width: "320px", textAlign: "center"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia, nisl eu rhoncus auctor, lacus libero tincidunt dui, in consectetur lorem arcu ut ex. Praesent tristique nisi quis metus rutrum sagittis. Lorem ipsum dolor sit amet,</h6>
-				</div>
-				<div className={styles.team} style = {{display: "flex", gap: "20px", flexDirection: "column"}}>
-							<div>
-								<div style = {{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-									<div style = {{textAlign: 'center'}}>
-										<hr />
-										<h1>Team 2022 - 2023</h1>
-										<h3 style = {{width: "300px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia, nisl eu rhoncus auctor, lacus libero tincidunt dui, in consectetur lorem arcu ut ex. Praesent tristique nisi quis metus rutrum sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis pharetra mi. Curabitur et metus congue, aliquet metus sit amet, condimentum elit. Sed accumsan risus lacinia turpis accumsan pharetra.</h3>
-									</div>
-									<div className={styles.profile }>
-										{team.exeMembers.map(member => <p><AceCard pic = {member.pic} name = {member.name.toUpperCase()} position = {member.position} bio = {member.bio}/></p>)}
-									</div>
-								</div>
-							</div>	
-							<div>
-								<div style = {{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-									<div style = {{textAlign: 'center'}}>
-										<h1>Design Team</h1>
-										<h3 style = {{width: "300px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia, nisl eu rhoncus auctor, lacus libero tincidunt dui, in consectetur lorem arcu ut ex. Praesent tristique nisi quis metus rutrum sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis pharetra mi. Curabitur et metus congue, aliquet metus sit amet, condimentum elit. Sed accumsan risus lacinia turpis accumsan pharetra.</h3>
-									</div>
-									<div className={styles.profile }>
-										{team.desTeam.map(member => <p><AceCard pic = {member.pic} name = {member.name.toUpperCase()} position = {member.position} bio = {member.bio}/></p>)}
-									</div>
-								</div>
-							</div>	
-							<div>
-								<div style = {{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-									<div style = {{textAlign: 'center', color: 'black'}}>
-										<h1>Web Team</h1>
-										<h3 style = {{width: "300px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia, nisl eu rhoncus auctor, lacus libero tincidunt dui, in consectetur lorem arcu ut ex. Praesent tristique nisi quis metus rutrum sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis pharetra mi. Curabitur et metus congue, aliquet metus sit amet, condimentum elit. Sed accumsan risus lacinia turpis accumsan pharetra.</h3>
-									</div>
-									<div className={styles.profile }>
-										{team.webTeam.map(member => <p><AceCard pic = {member.pic} name = {member.name.toUpperCase()} position = {member.position} bio = {member.bio}/></p>)}
-									</div>
-								</div>
-							</div>	
-						</div>
-			</>
-		)
-	}
+			</div>			
+	)
 }
 
 export default Ace;

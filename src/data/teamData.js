b0470414b722d14c7560710eea19e9b195286005
Ajay12338 <77118718+Ajay12338@users.noreply.github.com>
2023-03-20 10:24:26 +0530

    "Added bio(one-liner) and Linkedin Details"

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -4,127 +4,171 @@ const team = {
 			"name":"Ritunjay V.",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "President",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "Life is a killer !! Kill it, before it kills you !!",
+			"linkedin":"http://linkedin.com/in/ritunjay-m-b8b834116"
+			
 		},
 		{
 			"name":"Allen Manoj",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Vice President",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "Not brownie 😏",
+			"linkedin":"https://www.linkedin.com/in/allenmanoj"
 		},
 		{
 			"name":"Nivedita P.",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Vice President",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "Cold Comfort for Change",
+			"linedin":"https://www.linkedin.com/in/nivedita-palanisamy-4255631b4"
+
+		},
+		{
+			"name":"Shivanee Ramesh",
+			"pic":"https://drive.google.com/file/d/114zoHkfE8kdF1678DBWwz6SrN_d-Sgx1/view?usp=share_link",
+			"position":"Vice President",
+			"bio":"Weird is good!",
+			"linkedin":"http://www.linkedin.com/in/shivaneeramesh"
 		},
 		{
 			"name":"Swetha A.",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Secretary",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "There is no Ctrl-Z in life,when all else fails,just reboot!",
+			"linkedin":"https://www.linkedin.com/in/swetha-a-14ab62226"
+
 		},
 		{
 			"name":"Adithyarajen R.",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Treasurer",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "Cars, Games, Techs, Adrenaline Pumping, Travelling, Music (No particular order)",
+			"linkedin":"https://www.linkedin.com/in/adithyarajen/"
+
 		},
 		{
 			"name":"Aadesh Vijayaraghavan",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Operations Head",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "If you want Peace, Prepare for War.",
+			"linkedin":"https://www.linkedin.com/in/aadesh-vijayaraghavan-255133129/"
+
 		},
 		{
 			"name":"Sahana B.",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Joint secretary",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "An outgoing person having great appetite to unearth new challenges and experiences.",
+			"linkedin":"https://www.linkedin.com/in/sahana-balaji-32366b240"
+
 		},
 		{
 			"name":"Pooja T.",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Executive Member",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "When nothing goes right, go left.",
+			"linkedin":"https://www.linkedin.com/in/pooja-thirumurugan-744479200"
+
 		},
 		{
 			"name":"Preethi Purnimaa K",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Executive Member",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "I am a hard-working and driven individual who is always ready to face the challenges and work on it",
+			"linkedin":"www.linkedin.com/in/preethi-purnimaa-k-15b170256"
+
 		},
 		{
 			"name":"Karthick K",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Executive Member",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "Is it a bird? Is it a plane? No, it's a runtime error.",
+			"linkedin":"https://www.linkedin.com/in/karthick-k-90316a247/"
+
 		},
 		{
 			"name":"Swetha Shakthi U",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Executive Member",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "Always end up where I’m meant to be. 💫",
+			"linkedin":"https://www.linkedin.com/mwlite/in/swetha-sakthi-990297213"
+
 		},
 		{
 			"name":"Yaswanth Krishnan U",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Executive Member",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "When I have a camera in my hand, I know no fear.",
+			"linkedin":"https://www.linkedin.com/in/yashwanth-krishnan-299247220"
+
 		},
 		{
 			"name":"Hema A",
-			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
-			"position": "Executive Member",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"pic":"C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
+			"position":"Executive Member",
+			"bio":"I’m not perfect, but stories are always better with a touch of imperfection!",
+			"linkedin":"https://www.linkedin.com/in/hema-a-289475229"
+
 		},
 		{
 			"name":"Dharani Narumugai R",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Executive Member",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "Workaholic and Hakuna Matata ",
+			"linkedin":"https://www.linkedin.com/in/dharani-narumugai-ramprasath-655b8120b"
+
 		},
 		{
 			"name":"Adithya Vikas A",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Executive Member",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "Change Maker with a penchant for Puzzles",
+			"linkedin":"https://www.linkedin.com/in/adithya-vikas-ln/"
+
 		},
 		{
 			"name":"Aparajitha P",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Executive Member",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "A minimum 3 coffees a day to function kinda person.",
+			"linkedin":"www.linkedin.com/in/aparajitha-p-42271a108"
+
 		},
 		{
 			"name":"Naafiah Yusra A",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Executive Member",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "Passionate about my work.",
+			"linkedin":"https://www.linkedin.com/in/naafiah-yusra-a-1616a122a"
+
 		},
 		{
 			"name":"Sharan Balaji S",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Executive Member",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "I believe in myself while doing each and everything!",
+			"linkedin":"www.linkedin.com/in/sharan-balaji-s-34223022a"
 		},
 		{
 			"name":"Sanjana J",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Executive Member",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "In the end we'll all become stories.",
+			"linkedin":"https://www.linkedin.com/in/sanjana-jeyagopi-952682237/"
 		},
 		{
 			"name":"Siva Ishwarya B",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Executive Member",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "UKIYO",
+			"linkedin":"https://www.linkedin.com/in/siva-ishwarya-b-908a3a22b"
 		},
 		{
 			"name":"Vishnuvardan A",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Executive Member",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "I do not give up until I achieved it",
+			"linkedin":"https://www.linkedin.com/in/vishnuvardan-a-264656251"
 		}
     ],
 
@@ -133,49 +177,58 @@ const team = {
 			"name":"Vempali Dharani",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Design Team",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
-		},
-		{
-			"name":"Sibi Mughil T",
-			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
-			"position": "Design Team",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "Eagerness in learning ",
+			"linkedin":"https://www.linkedin.com/public-profile/settings"
 		},
 		{
 			"name":"Taruna Indrajith",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Design Team",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "Beautiful things don't ask for attention ",
+			"linkedin":"https://www.linkedin.com/public-profile/settings"
+		},
+		{
+			"name":"SIBI MUGHIL T",
+			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
+			"position": "Design Team",
+			"bio": "I’m the most awesome person I know.",
+			"linkedin":"https://www.linkedin.com/in/sibimughil12132002/"
 		},
 		{
 			"name":"Shruthika S",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Design Team",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "Fun",
+			"linkedin":"https://www.linkedin.com/in/shruthika-s-b69202237"
+
 		},
 		{
 			"name":"Soorya B",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Design Team",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "Knowledge gaining enthusiast, into ideating visions.s.",
+			"linkedin":"https://www.linkedin.com/in/soorya-b-0405241a9"
 		},
 		{
 			"name":"Harini S",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Design Team",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "I'm ambitious and always have a positive mind set.",
+			"linkedin":"https://www.linkedin.com/in/harini-s-7712b4256"
 		},
 		{
 			"name":"Stany Romero S",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Design Team",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "I take a lot of pride in being myself. I'm comfortable with who I am.",
+			"linkedin":"www.linkedin.com/in/stany-romero-ab0871222/"
 		},
 		{
 			"name":"Manoj Kumar S",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Design Team",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "I am a kind of person who is interested in technical line.",
+			"linkedin":"https://www.linkedin.com/in/manoj-kumar-ba6154256"
 		}
 	],
 
@@ -184,31 +237,36 @@ const team = {
 			"name":"Shiva Sankari C",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Web Team",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": " I believe my dream is the best guide for my life",
+			"linkedin":"https://www.linkedin.com/in/shiva-sankari-c-339b441b2"
 		},
 		{
 			"name":"Ajay S",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Web Team",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "I am a passionate and hardworking student.",
+			"linkedin":"https://www.linkedin.com/in/ajay-sam-805266255"
 		},
 		{
 			"name":"Lakshmipriya R",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Web Team",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "Embrace the Randomness ✨",
+			"linkedin":"https://www.linkedin.com/in/lakshmipriya-r-b32770201"
 		},
 		{
 			"name":"Dhanrithii D",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Web Team",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "Challenges never scare me, not trying does. ",
+			"linkedin":"https://www.linkedin.com/in/dhanrithii-d-017171226/"
 		},
 		{
 			"name":"Deepak Srinivas M",
 			"pic": "C:/Users/socce/OneDrive/Desktop/wallpapers/fin.jpg/",
 			"position": "Web Team",
-			"bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam volutpat velit vitae dapibus. Donec convallis nunc a neque dictum aliquet a a arcu. Orci varius natoque penatibus et magnis."
+			"bio": "I put the Pro in Programming",
+			"linkedin":"https://www.linkedin.com/mwlite/in/deepak-srinivas-80239922a"
 		}
     ]
 }
@@ -217,6 +275,12 @@ const team = {
 export default function getTeam(){
     return team;
 }
+
+
+
+
+
+
 
 
 

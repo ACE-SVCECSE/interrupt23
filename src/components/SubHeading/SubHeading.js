70f920a39020aa084741740e36ee64e39f6aaa1a
Shiva Sankari C <shivasankari3101@gmail.com>
2023-03-24 18:17:53 +0530

    Minor changes

--- src/components/SubHeading/SubHeading.js
+++ src/components/SubHeading/SubHeading.js
@@ -6,8 +6,7 @@ function SubHeading () {
         <div className={styles.subHeading}>
             <h1 className={styles.heading}>INTERRUPT '23</h1>
             <p className={styles.oneliner }>The Department of Computer Science and Engineering, SVCE, and ACE jointly present Interrupt, a national-level technical symposium where talents meet the limelight they deserve to acknowledge and showcase their finest selves.
-            It offers a wide range of activities, such as workshops, coding contests, ideathons, web development competitions, and many more. Additionally, it provides a forum for students to gain current technical skills through practical workshops led by field experts.
-            INTERRUPT'21 was a big success by digitally connecting tech enthusiasts together, following up on the prior offline event, INTERRUPT'19.</p>
+            It offers a wide range of activities, such as workshops, coding contests, ideathons, web development competitions, and many more.</p>
             <button>View More</button>
         </div>
     </>
--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -256,32 +256,7 @@ Additional credits will be given for showcasing creative skills like animations 
                 "Manoj Kumar S" : "+91 94444 31042"
             }
         }
-    },
-    {
-        id: 10,
-        title: "Workshop",
-        oneliner: "One Liner 1 - Lorem ipsum dolor sit amet consectetur. Porttitor purus amet libero nibh vel dignissim.",
-        image: "https://thumbs.dreamstime.com/b/technical-support-customer-service-business-technology-internet-concept-154148058.jpg",
-        eventDetails: {
-            teamSize: 3,
-            rules:[
-                "rule1",
-                "rule1",
-                "rule2"
-            ],
-            rounds:[
-                "Lorem ipsum dolor",
-                "Lorem ipsum dolor"
-            ],
-            judging: "Lorem ipsum dolor",
-            deadline:"5th May 2021, 11:59 p.m",
-            registerLink: "",
-            contact: {
-                "Karthick.K:" : "+91 94448 53879",
-                "Vishnuvardan.A" : "+91 72992 13502"
-            }
-        }
-    }  
+    }
 ];
 
 export default function getEvents(){
